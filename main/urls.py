fd6a5b69f2d8a62e6f39fcea89eb6e3e415422dc
fauzanjaisyu <fauzanjaisyu@gmail.com>
2023-11-22 15:48:49 +0700

    add cors headers

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import add_product_ajax, get_product_json, show_stock, create_product, show_xml, show_json, show_json_by_id, show_xml_by_id, register, login_user, logout_user
+from main.views import add_product_ajax, create_product_flutter, get_product_json, show_stock, create_product, show_xml, show_json, show_json_by_id, show_xml_by_id, register, login_user, logout_user
 
 app_name = 'main'
 
@@ -15,4 +15,5 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('get-item/', get_product_json, name='get_product_json'),
     path('create-ajax/', add_product_ajax, name='add_product_ajax'),
+    path('create-flutter/', create_product_flutter, name='create_product_flutter'),
 ]
